b0cb4f34a12727f7e1a0d9d64e7af761283d7dd9
Scott Ammon <scott.o.ammon@gmail.com>
2018-08-07 08:44:55 -0700

    fixed footer and required fields in signup

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,38 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-};
-
-function Footer(props) {
-  const { classes } = props;
+const Footer = props => {
   return (
-    <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Toolbar>
-          <Typography variant="title" color="inherit">
-            <div>
-              <a className="footer-link" href="#">FAQ</a>
-              <a className="footer-link" href="#">Help</a>
-              <a className="footer-link" href="#">Contact</a>
-              <a className="footer-link" href="#">Trip Planner</a>
-            </div>
-          </Typography>
-        </Toolbar>
-      </AppBar>
+    <div className="footer-container">
+      <a className="footer-link" href="#">FAQ</a>
+      <a className="footer-link" href="#">Help</a>
+      <a className="footer-link" href="#">Contact</a>
+      <a className="footer-link" href="#">Trip Planner</a>
     </div>
   );
 }
 
-Footer.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(Footer);
+export default Footer;
