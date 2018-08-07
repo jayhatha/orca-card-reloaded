8c966841fe20714c08335c0895ad8cdc2f36fd99
Scott Ammon <scott.o.ammon@gmail.com>
2018-08-07 08:56:57 -0700

    added to footer

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -3,10 +3,21 @@ import React from 'react';
 const Footer = props => {
   return (
     <div className="footer-container">
-      <a className="footer-link" href="#">FAQ</a>
-      <a className="footer-link" href="#">Help</a>
-      <a className="footer-link" href="#">Contact</a>
-      <a className="footer-link" href="#">Trip Planner</a>
+      <div className="footer-top">
+        <a className="footer-link" href="#">FAQ</a>
+        <a className="footer-link" href="#">Help</a>
+        <a className="footer-link" href="#">Contact</a>
+        <a className="footer-link" href="#">Trip Planner</a>
+      </div>
+      <div className="footer-bottom">
+        <div>
+          <a className="" href="#">Terms of Use</a>
+          <a className="" href="#">Privacy Policy</a>
+        </div>
+        <div>
+          <p>2018 GA | All Rights Reserved</p>
+        </div>
+      </div>
     </div>
   );
 }
