62824d145c9dc32ca760f60ab2fc9b4593317eb4
jayhatha <jay@jayhathaway.com>
2018-08-07 12:01:37 -0700

    more redux

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -6,7 +6,9 @@ const initialState = {token: '', user: null};
 const userReducer = (state = initialState, action) => {
 	switch(action.type) {
 		case UPDATE_USER:
-			console.log('received the test action')
+			console.log('received the test action, here is the user: ' + action.token, action.user)
+			var objAss = Object.assign({}, state, {token: action.token, user: action.user})
+			console.log(objAss)
 			return Object.assign({}, state, {token: action.token, user: action.user})
 		default:
 			return state
