0f14bba7252a5cb0a476e8d8593c65558220bde6
jayhatha <jay@jayhathaway.com>
2018-08-07 20:31:51 -0700

    fixed card layout

--- client/src/Cards.js
+++ client/src/Cards.js
@@ -3,21 +3,27 @@ import React from 'react';
 const Cards = props => {
   return (
     <div>
-    <div className='card-container'>
-      <div className='card-nickname'><h3>Scott's Card</h3></div>
-      <div className='card-top'>
-        <h2>BALANCE</h2>
+      <div className='card-container'>
+        <div className='card-nickname'><h3>Scott's Card</h3></div>
+        <div className='card-top'>
+          <h2>BALANCE</h2>
+        </div>
+        <div className='card-balance'>
+          <h1>$50</h1>
+        </div>
+        <div>
+        <span className='card-serial'>0111001</span>
+        <span className='card-passes'>
+          PUGETPASS SEP18 $2.75
+        </span>
+        <span className='card-passes'>
+          AUTO-RELOAD $25
+        </span>
       </div>
-      <div className='card-balance'>
-        <h1>$50</h1>
+
       </div>
-      <div className='card-bottom'>
-        <div className='card-serial'>0111001</div>
-        <div className='card-passes'><p>AUTO-LOAD: $25</p><p>PUGETPASS SEP18 $2.75</p></div>
-      </div>
+      <div className='card-buttons'>auto-load / pass / reload buttons go here</div>
     </div>
-    <div className='card-buttons'>auto-load / pass / reload buttons go here</div>
-  </div>
   );
 };
 
