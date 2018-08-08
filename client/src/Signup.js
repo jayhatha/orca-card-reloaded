d2645d2ed27d19e410951dd9f6db89233d921015
Scott Ammon <scott.o.ammon@gmail.com>
2018-08-08 16:22:43 -0700

    editing signup fields

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -12,142 +12,153 @@ const Signup = props => {
           <h4>Sign up now to get all the benefits of Orca!</h4>
         </div>
         <form onSubmit={props.handleSubmit}>
-          <TextField
-            id="first"
-            name="first"
-            type="text"
-            label="First Name"
-            className="inputField"
-            value={props.first}
-            onChange={props.handleInputChange}
-            margin="normal"
-            required
-          /><br />
-          <TextField
-            id="last"
-            name="last"
-            type="text"
-            label="Last Name"
-            className="inputField"
-            value={props.last}
-            onChange={props.handleInputChange}
-            margin="normal"
-            required
-          /><br />
-          <TextField
-            id="username"
-            name="username"
-            type="text"
-            label="Username"
-            className="inputField"
-            value={props.username}
-            onChange={props.handleInputChange}
-            margin="normal"
-            required
-          /><br />
-          <TextField
-            id="email"
-            name="email"
-            type="text"
-            label="Email"
-            className="inputField"
-            value={props.email}
-            onChange={props.handleInputChange}
-            margin="normal"
-            required
-          /><br />
-          <TextField
-            id="phone"
-            name="phone"
-            type="text"
-            label="Phone"
-            className="inputField"
-            value={props.phone}
-            onChange={props.handleInputChange}
-            margin="normal"
-            required
-          /><br />
-          <p>Date of Birth:</p>
-          <TextField
-            id="dob"
-            name="dob"
-            type="date"
-            className="inputField"
-            value={props.dob}
-            onChange={props.handleInputChange}
-            margin="normal"
-            required
-          /><br />
-          <p>Address</p>
-          <TextField
-            id="street"
-            name="street"
-            type="text"
-            label="Street"
-            className="inputField"
-            value={props.street}
-            onChange={props.handleInputChange}
-            margin="normal"
-            required
-          /><br />
-          <TextField
-            id="city"
-            name="city"
-            type="text"
-            label="City"
-            className="inputField"
-            value={props.city}
-            onChange={props.handleInputChange}
-            margin="normal"
-            required
-          /><br />
-          <TextField
-            id="state"
-            name="state"
-            type="text"
-            label="State"
-            className="inputField"
-            value={props.state}
-            onChange={props.handleInputChange}
-            margin="normal"
-            required
-          /><br />
-          <TextField
-            id="zip"
-            name="zip"
-            type="text"
-            label="Zipcode"
-            className="inputField"
-            value={props.zip}
-            onChange={props.handleInputChange}
-            margin="normal"
-            required
-          /><br />
-          <p>Choose a secret question:</p>
-          <SimpleSelect></SimpleSelect><br />
-          <TextField
-            id="answer"
-            name="answer"
-            type="text"
-            label="Answer"
-            className="inputField"
-            value={props.answer}
-            onChange={props.handleInputChange}
-            margin="normal"
-            required
-          /><br />
-          <TextField
-            id="password"
-            name="password"
-            type="password"
-            label="Password"
-            className="inputField"
-            value={props.password}
-            onChange={props.handleInputChange}
-            margin="normal"
-            required
-          /><br />
-          <Button id="button" type="submit" value="Sign up">Sign up</Button>
+          <div className="signup-section">
+            <h3>Cardholder Details</h3>
+            <TextField
+              id="first"
+              name="first"
+              type="text"
+              label="First Name"
+              className="inputField"
+              value={props.first}
+              onChange={props.handleInputChange}
+              margin="normal"
+              required
+            /><br />
+            <TextField
+              id="last"
+              name="last"
+              type="text"
+              label="Last Name"
+              className="inputField"
+              value={props.last}
+              onChange={props.handleInputChange}
+              margin="normal"
+              required
+            /><br />
+            <p className="fade-title">Date of Birth:</p>
+            <TextField
+              id="dob"
+              name="dob"
+              type="date"
+              className="inputField"
+              value={props.dob}
+              onChange={props.handleInputChange}
+              margin="normal"
+              required
+            /><br />
+          </div>
+          <div className="signup-section">
+            <h3>Contact Details</h3>
+            <TextField
+              id="phone"
+              name="phone"
+              type="text"
+              label="Phone"
+              className="inputField"
+              value={props.phone}
+              onChange={props.handleInputChange}
+              margin="normal"
+              required
+            /><br />
+            <TextField
+              id="email"
+              name="email"
+              type="text"
+              label="Email"
+              className="inputField"
+              value={props.email}
+              onChange={props.handleInputChange}
+              margin="normal"
+              required
+            /><br />
+          </div>
+          <div className="signup-section">
+            <h3>Address</h3>
+            <TextField
+              id="street"
+              name="street"
+              type="text"
+              label="Street"
+              className="inputField"
+              value={props.street}
+              onChange={props.handleInputChange}
+              margin="normal"
+              required
+            /><br />
+            <TextField
+              id="city"
+              name="city"
+              type="text"
+              label="City"
+              className="inputField"
+              value={props.city}
+              onChange={props.handleInputChange}
+              margin="normal"
+              required
+            /><br />
+            <TextField
+              id="state"
+              name="state"
+              type="text"
+              label="State"
+              className="inputField"
+              value={props.state}
+              onChange={props.handleInputChange}
+              margin="normal"
+              required
+            /><br />
+            <TextField
+              id="zip"
+              name="zip"
+              type="text"
+              label="Zipcode"
+              className="inputField"
+              value={props.zip}
+              onChange={props.handleInputChange}
+              margin="normal"
+              required
+            /><br />
+          </div>
+          <div className="signup-section">
+            <h3>My ORCA Login Details</h3>
+            <p className="fade-title">Choose a secret question:</p>
+            <SimpleSelect></SimpleSelect><br />
+            <TextField
+              id="answer"
+              name="answer"
+              type="text"
+              label="Answer"
+              className="inputField"
+              value={props.answer}
+              onChange={props.handleInputChange}
+              margin="normal"
+              required
+            /><br />
+            <TextField
+              id="username"
+              name="username"
+              type="text"
+              label="Username"
+              className="inputField"
+              value={props.username}
+              onChange={props.handleInputChange}
+              margin="normal"
+              required
+            /><br />
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              className="inputField"
+              value={props.password}
+              onChange={props.handleInputChange}
+              margin="normal"
+              required
+            /><br />
+          </div>
+          <Button className="signup-button" id="button" type="submit" value="Sign up">Sign up</Button>
         </form>
         <p className="alert-msg">{(props.response) ? props.response.message : ''}</p>
       </div>
