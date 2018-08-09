0583a0a08a5d5b3c1f21b0782bff3d202205b44e
Scott Ammon <scott.o.ammon@gmail.com>
2018-08-09 15:35:25 -0700

    updating home to change link to my cards when logged in

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,25 +1,23 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-import {withRouter} from 'react-router-dom';
 
-class Home extends Component {
+const Home = props => {
+  var goToRoute = props.user.first ? "/profile" : "/login"
 
-  render() {
-    return (
-      <div className="home">
-        <h3>Welcome to Orca!</h3>
-        <div class="have-card">
-          <h4>Have an Orca Card?</h4>
-          <Button id="button" component={Link} to="/login" className="button">GO TO MY CARDS</Button>
-        </div>
-        <div>
-          <h4>Need a Card?</h4>
-          <Button id="button" component={Link} to="/getcard" className="button">GET A CARD</Button>
-        </div>
+  return (
+    <div className="home">
+      <h3>Welcome to Orca!</h3>
+      <div class="have-card">
+        <h4>Have an Orca Card?</h4>
+        <Button id="button" component={Link} to={goToRoute} className="button">GO TO MY CARDS</Button>
       </div>
-    );
-  }
+      <div>
+        <h4>Need a Card?</h4>
+        <Button id="button" component={Link} to="/getcard" className="button">GET A CARD</Button>
+      </div>
+    </div>
+  );
 }
 
 export default Home
