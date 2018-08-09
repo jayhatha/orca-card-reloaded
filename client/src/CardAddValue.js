37c6c13975a2f1d4894799accb0dfae9f28835d7
Scott Ammon <scott.o.ammon@gmail.com>
2018-08-09 11:02:13 -0700

    adding card value pages

--- client/src/CardAddValue.js
+++ client/src/CardAddValue.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import {withRouter} from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
-class CardAddValue extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
+const CardAddValue = props => {
+    console.log('props are: ', props)
     return (
       <div className="form-container">
        <h2>Add Value To Your Orca Card</h2>
-       <h3>Current Balance:</h3><span>{this.props.user.balance}</span>
+       <h3>{props.user.first}'s Card: {props.card.id}</h3>
+       <h3>Current Balance:</h3><span>{props.card.balance}</span>
        <h3>Amount to Add:</h3>
-       <form onSubmit={this.props.handleSubmit}>
+       <form onSubmit={props.handleSubmit}>
         <TextField
             name="balance"
             type="text"
@@ -24,10 +21,10 @@ class CardAddValue extends Component {
             margin="normal"
             required
           />
+        <button id="button">Add Value</button>
         </form>
       </div>
     )
-  }
 }
 
 export default withRouter(CardAddValue)
--- client/src/CardBalanceContainer.js
+++ client/src/CardBalanceContainer.js
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
 import CardAddValue from './CardAddValue';
+import axios from 'axios';
+import store from './store';
+import { updateCard} from './actions/index';
+import { connect } from 'react-redux';
+
+const mapDispatchToProps = {
+  updateCard
+}
+
+const mapStatetoProps = state => {
+  return {
+    user: state.user,
+    card: state.card
+  }
+}
 
 class CardBalanceContainer extends Component {
   constructor(props) {
     super(props)
+    this.handleSubmitValue = this.handleSubmitValue.bind(this)
+    this.handleSubmitAutoReload = this.handleSubmitAutoReload.bind(this)
+  }
+
+  handleSubmitValue() {
+
+  }
+
+  handleSubmitAutoReload() {
+
   }
 
   render() {
-    return(
-      <div></div>
+    return (
+      <div>
+        <CardAddValue user={this.props.user} card={this.props.card}/>
+      </div>
     )
   }
 }
 
-export default CardBalanceContainer
+export default connect(mapStatetoProps, mapDispatchToProps)(CardBalanceContainer)
